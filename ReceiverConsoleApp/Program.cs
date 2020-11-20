7a85db80bece79390b27c8839b8ff88eee4cb840
y10e <y10exxx@gmail.com>
2020-11-20 15:02:33 +0900

    add project file

---
+++ ReceiverConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReceiverConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                using (var stream = new NamedPipeClientStream("NamedPipe"))
+                {
+                    await stream.ConnectAsync();
+
+                    using (var reader = new StreamReader(stream))
+                    {
+                        while (stream.IsConnected)
+                        {
+                            var str = await reader.ReadLineAsync();
+                            Console.WriteLine("Data :" + str);
+                            Thread.Sleep(100);
+                        }
+                    }
+                }
+            }).Wait();
+        }
+    }
+}
---
+++ SenderConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SenderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a message, and then press Enter.");
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    using (var stream = new NamedPipeServerStream("NamedPipe"))
+                    {
+                        await stream.WaitForConnectionAsync();
+
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            writer.AutoFlush = true;
+                            while (true)
+                            {
+                                var data = Console.ReadLine();
+                                await writer.WriteLineAsync(data);
+                            }
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("Connection is closed.");
+                }
+            }).Wait();
+
+            Console.WriteLine("End of sample.");
+            Console.ReadLine();
+        }
+    }
+}
